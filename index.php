dcba2fcfdbd15e9588a992da6baa002e8509c096
Matvey <volko-matvey@yandex.ru>
2019-02-06 23:39:34 +0500

    tabulation

--- index.php
+++ index.php
@@ -3,41 +3,41 @@
 $db = new mysqli('localhost', 'root','', 'test_internet_projects');
 $query="SELECT * FROM username left outer join tel ON username.id=tel.id left outer join dob on username.id=dob.id";
 
-	$result = $db->query($query);
-	while($val = $result->fetch_assoc()) {
-		$users[]=$val;
-	}
+$result = $db->query($query);
+while($val = $result->fetch_assoc()) {
+	$users[]=$val;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Test</title>
-  <link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-  	<header>
-    	<div class="container">
-      		<div class="logo">Тест</div>
-    	</div>
-  	</header>
-  	<div class="container">
-  		<table class="table">
-				<tr>
-					<th>Имя</th>
-					<th>Фамилия</th>
-					<th>Дата рождения</th>
-					<th>Номер телефона</th>
-				</tr>
-				<?php foreach ($users as $value): ?>
+	<header>
+		<div class="container">
+			<div class="logo">Тест</div>
+		</div>
+	</header>
+	<div class="container">
+		<table class="table">
+			<tr>
+				<th>Имя</th>
+				<th>Фамилия</th>
+				<th>Дата рождения</th>
+				<th>Номер телефона</th>
+			</tr>
+			<?php foreach ($users as $value): ?>
 				<tr>
 					<td><?=$value['name']?></td>
 					<td><?=$value['surname']?></td>
 					<td><?=$value['dob']?></td>
 					<td><?=$value['tel']?></td>
 				</tr>
-				<?php endforeach; ?>
-			</table>
+			<?php endforeach; ?>
+		</table>
 	</div>
 </body>
 
--- request.php
+++ request.php
@@ -7,15 +7,15 @@ $arr[0][tel]='89824810000';
 $arr[1][name]='Сидор';
 $arr[1][surename]='Сидоров';
 $arr[1][tel]='89824810000';*/
- $values = "";
+$values = "";
 
 for($i = 0; $i < count($arr); $i++){
 
-    if($values != "")
-      $values .= ",";
+	if($values != "")
+		$values .= ",";
 
-    $values .= "('".$arr[$i][name]."', '".$arr[$i][surename]."', ".$arr[$i][tel].")";
-  }
+	$values .= "('".$arr[$i][name]."', '".$arr[$i][surename]."', ".$arr[$i][tel].")";
+}
 
 $q="INSERT INTO user (name,surename,tel) VALUES $values";
 echo $q;
